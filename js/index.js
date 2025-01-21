3128747493940fadae7124ba148bb55c8ede1242
Ciara G <166245306+ciaragray@users.noreply.github.com>
2025-01-20 21:01:13 -0600

    fixed javascript

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
+//creating footer
 const body = document.body;
 const footer = document.createElement("footer");
-footer.innerHTML = `<P>&copy; ${new Date().getFullYear()} Ciara Grayson's Portfolio | All Rights Reserved</P>`;
+const today = new Date();
+const thisYear = today.getFullYear();
+footer.innerHTML = `<P>&copy; ${thisYear} Ciara Grayson's Portfolio | All Rights Reserved</P>`;
 body.appendChild(footer);
-const skills = ["Javascript", "HTML", "CSS", "SQL","Github"];
+// creating skills
+const skills = ["Javascript", "HTML", "CSS", "SQL", "Github"];
 const skillsSection = document.querySelector("#Skills");
-const skillsList = skillsSection.querySelector("ul"); 
-for(let i = 0; i<skills.length; i++) {
-    const skill= document.createElement("li");
-    skill.innerText= skills[i];
-    skillsList.appendChild(skill);
+const skillsList = skillsSection.querySelector("ul");
+for (let i = 0; i < skills.length; i++) {
+  const skill = document.createElement("li");
+  skill.innerText = skills[i];
+  skillsList.appendChild(skill);
 }
